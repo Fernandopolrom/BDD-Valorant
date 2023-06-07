d8183b9f81e7999c1c289478ef655f205edb710b
Fernandopolrom <117643005+Fernandopolrom@users.noreply.github.com>
2023-06-07 19:00:22 +0200

    Update Valorant-schema.sql

--- Valorant-schema.sql
+++ Valorant-schema.sql
@@ -1,4 +1,10 @@
-CREATE DATABASE `valorant` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+
+
+DROP DATABASE IF EXISTS valorant;
+
+CREATE DATABASE valorant CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
+
+use valorant;
 
 CREATE TABLE `agentes` (
   `id_agente` int NOT NULL,
@@ -20,23 +26,13 @@ CREATE TABLE `armas` (
   PRIMARY KEY (`id_arma`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-CREATE TABLE `jugadores` (
-  `id_jugador` int NOT NULL,
-  `nombre` text,
-  `edad` int DEFAULT NULL,
-  `país` text,
-  `rango` enum('hierro','bronce','plata','oro','platino','diamante','ascendente','inmortal','radiante') DEFAULT NULL,
-  `id_agente` int DEFAULT NULL,
+CREATE TABLE `temporada` (
+  `episodio` int NOT NULL,
   `id_partida` int DEFAULT NULL,
-  `id_arma` int DEFAULT NULL,
-  `puntuacion` int DEFAULT NULL,
-  PRIMARY KEY (`id_jugador`),
-  KEY `id_agente` (`id_agente`),
-  KEY `id_partida` (`id_partida`),
-  KEY `id_arma` (`id_arma`),
-  CONSTRAINT `jugadores_ibfk_1` FOREIGN KEY (`id_agente`) REFERENCES `agentes` (`id_agente`),
-  CONSTRAINT `jugadores_ibfk_2` FOREIGN KEY (`id_partida`) REFERENCES `partidas` (`id_partida`),
-  CONSTRAINT `jugadores_ibfk_3` FOREIGN KEY (`id_arma`) REFERENCES `armas` (`id_arma`)
+  `fecha` date DEFAULT NULL,
+  `estado` text,
+  `id_jugador` int DEFAULT NULL,
+  PRIMARY KEY (`episodio`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 CREATE TABLE `partidas` (
@@ -55,6 +51,27 @@ CREATE TABLE `partidas` (
   CONSTRAINT `partidas_ibfk_1` FOREIGN KEY (`episodio`) REFERENCES `temporada` (`episodio`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+
+CREATE TABLE `jugadores` (
+  `id_jugador` int NOT NULL,
+  `nombre` text,
+  `edad` int DEFAULT NULL,
+  `país` text,
+  `rango` enum('hierro','bronce','plata','oro','platino','diamante','ascendente','inmortal','radiante') DEFAULT NULL,
+  `id_agente` int DEFAULT NULL,
+  `id_partida` int DEFAULT NULL,
+  `id_arma` int DEFAULT NULL,
+  `puntuacion` int DEFAULT NULL,
+  PRIMARY KEY (`id_jugador`),
+  KEY `id_agente` (`id_agente`),
+  KEY `id_partida` (`id_partida`),
+  KEY `id_arma` (`id_arma`),
+  CONSTRAINT `jugadores_ibfk_1` FOREIGN KEY (`id_agente`) REFERENCES `agentes` (`id_agente`),
+  CONSTRAINT `jugadores_ibfk_3` FOREIGN KEY (`id_arma`) REFERENCES `armas` (`id_arma`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+  
 CREATE TABLE `rol` (
   `id_rol_partida_jugador` int NOT NULL,
   `rol_principal` text,
@@ -67,11 +84,20 @@ CREATE TABLE `rol` (
   CONSTRAINT `rol_ibfk_2` FOREIGN KEY (`id_jugador`) REFERENCES `jugadores` (`id_jugador`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-CREATE TABLE `temporada` (
-  `episodio` int NOT NULL,
-  `id_partida` int DEFAULT NULL,
-  `fecha` date DEFAULT NULL,
-  `estado` text,
-  `id_jugador` int DEFAULT NULL,
-  PRIMARY KEY (`episodio`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
